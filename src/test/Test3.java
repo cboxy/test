581bf795499b304c98b7151788ef4c9ab03f1b6b
cboxy <cboxyo@gmail.com>
2013-02-20 23:10:40 +0800

    提交修改

--- src/test/Test3.java
+++ src/test/Test3.java
@@ -6,8 +6,8 @@ public class Test3 {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		System.out.println("Test 3333");
-		System.out.println("Test 4444");
+		System.out.println("Test aaa");
+		System.out.println("Test bbb");
 	}
 
 }
