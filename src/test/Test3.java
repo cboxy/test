0d1532b191dbe1f64d58792d875d54fa008d5c54
cboxy <cboxyo@gmail.com>
2013-02-20 23:08:41 +0800

    主线修改Test3
    修改行内字符

--- src/test/Test3.java
+++ src/test/Test3.java
@@ -6,8 +6,8 @@ public class Test3 {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		System.out.println("Test 3");
-		System.out.println("Test 4");
+		System.out.println("Test 3333");
+		System.out.println("Test 4444");
 	}
 
 }
